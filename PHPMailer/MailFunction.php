59e8405febbe970e38bf713642d5b7df2b40ac25
scaledesk <nkscoder@gmail.com>
2017-02-02 18:07:14 +0530

    gmail smtp setting update

--- PHPMailer/MailFunction.php
+++ PHPMailer/MailFunction.php
@@ -31,26 +31,26 @@ if ($comments) {
 
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'sub5.mail.dreamhost.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'hi@scaledesk.com';                 // SMTP username
-$mail->Password = 'qazplmq1w2e3r4';                       // SMTP password
-//$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Username = 'hello@kreativstreet.com';                 // SMTP username
+$mail->Password = '';                       // SMTP password
+$mail->SMTPSecure = 'TLS';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;// TCP port to connect to
 $mail->IsHTML(true);
-$mail->setFrom('hi@scaledesk.com', 'Kreativ');
+$mail->setFrom('hello@kreativstreet.com', 'Kreativ');
 //$mail->addAddress('ellen@example.com');               // Name is optional
-$mail->addReplyTo('hi@scaledesk.com', 'noreply');
+$mail->addReplyTo('hello@kreativstreet.com', 'noreply');
 
 $mail1->isSMTP();                                      // Set mailer to use SMTP
-$mail1->Host = 'sub5.mail.dreamhost.com';  // Specify main and backup SMTP servers
+$mail1->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail1->SMTPAuth = true;                               // Enable SMTP authentication
-$mail1->Username = 'hi@scaledesk.com';                 // SMTP username
-$mail1->Password = 'qazplmq1w2e3r4';                           // SMTP password
-//$mail1->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail1->Username = 'hello@kreativstreet.com';                 // SMTP username
+$mail1->Password = '';                           // SMTP password
+$mail1->SMTPSecure = 'TLS';                            // Enable TLS encryption, `ssl` also accepted
 $mail1->Port = 587;// TCP port to connect to
 $mail1->IsHTML(true);
-$mail1->setFrom('hi@scaledesk.com', 'Kreativ');
+$mail1->setFrom('hello@kreativstreet.com', 'Kreativ');
 
 /*if($_FILES['fileToUpload']){*/
 $mail1->AddAttachment($_FILES['fileToUpload']['tmp_name'], $_FILES['fileToUpload']['name']);
